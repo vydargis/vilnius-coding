16c9b05a8d688439b967f83db10d1e2f4f6f156e
DESKTOP-9S73P7O\pc <pol.vytautas.dargis@gmailcom>
2017-08-21 04:29:24 +0300

    no message

---
+++ homework/4-php/1/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+  function myNumber($num) {
+    if($num % 2 == 0) {
+      echo "$num is a even number.";
+    } else {
+    	echo "$num is not a even number.";
+    }
+  }
+
+  myNumber(4);
+?>
+
+
