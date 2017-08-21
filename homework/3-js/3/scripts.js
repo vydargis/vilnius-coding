2ab0796de22003f2ff5db7d9313e7eeab7bf5c47
DESKTOP-9S73P7O\pc <pol.vytautas.dargis@gmailcom>
2017-08-21 03:40:56 +0300

    no message

---
+++ homework/3-js/3/scripts.js
@@ -0,0 +1,21 @@
+// no validation
+
+$(function() {
+	$(".clean").click(function() {
+		$(".box").html("Thank you!");
+		$("#table, .container, h2").fadeOut(200, "linear");
+
+	});
+
+	$("#btn").click(function() {
+		var row = $("<tr></tr>");
+
+		var $nameRow = "<td>" + "Name: <br>" + $("#name").val() + "</td>";
+		var $surnameRow = "<td>" + "Surname: <br>" + $("#surname").val() + "</td>";
+		row.html($nameRow + $surnameRow);
+
+		$("#table").append(row);
+	});
+
+});
+
