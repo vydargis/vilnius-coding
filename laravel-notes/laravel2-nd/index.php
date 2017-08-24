8d8ff0de6770b6f9c778aa3a0e3a35a769039c2a
DESKTOP-9S73P7O\pc <pol.vytautas.dargis@gmailcom>
2017-08-24 12:11:38 +0300

    no message

---
+++ laravel-notes/laravel2-nd/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$names = [
+  'Doe',
+  'Mary',
+  'Tom'
+];
+
+// loop who iterate over arrays. works only on arrays and objects
+//
+
+
+require 'index.view.php';
---
+++ laravel-notes/laravel2-nd/index.view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+
+    <ul>
+      <?php
+        foreach ($names as $name) {
+          echo "<li>$name</li>";
+        }
+      ?>
+    </ul>
+
+
+  </body>
+</html>
