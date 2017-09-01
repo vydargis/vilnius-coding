8ed2360691a416c95843a7933e2fdda491460749
Vytautas Dargis <vy.dargis@gmail.com>
2017-09-01 18:49:54 +0300

    Update oop-basic.php

--- php-notes/oop_basics/oop-basic.php
+++ php-notes/oop_basics/oop-basic.php
@@ -1,13 +1,20 @@
-<!--
+
 * Objects - code grouped by common theme.
+
 * Abstractions of code structures:
   a) functions
   b) attributes
 * Objects gives code organiztion and maintainability.
+
 * Add clarity, reduce complexity.
+
 * Simple rules allow complex interactions.
+
 * Emphasizes data over procedure.
+
 * Code modularity.
+
 * Code resuability
+
 * Well-suited for databases.
--->
+
